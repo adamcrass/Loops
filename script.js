91f0dc7b153072cebc460dcfe6d1bc108c2ffec4
Adam Crass <aj.crass@gmail.com>
2025-03-18 10:31:55 -0700

    Finished

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const number = 0;
-console.log(number);
+let number1 = 0;
+let number2 = 0;
 
-while (number < 10) {
-  number + 1 == number;
-  console.log(number);
+while (number1 < 10) {
+  number2 = number1 + 1;
+  console.log(number1);
+  number1 = number2 + 1;
+  console.log(number2);
 }
 
+console.log(number1);
 console.log("Your number is 10!");
+
+let num1 = 0;
+let num2 = 0;
+
+console.log(num1);
+
+num1 = num2 + 1;
+
+while (num1 < 9) {
+  num2 = num1 + 2;
+  console.log(num1);
+  num1 = num2 + 2;
+  console.log(num2);
+}
+
+console.log(num1);
+console.log("Your number is 11!");
+
+for (let thing1 = 0; thing1 <= 10; thing1 = thing1 + 1) {
+  console.log(thing1);
+}
+
+console.log("Third project done!");
+
+for (let thing2 = 1; thing2 <= 11; thing2 = thing2 + 2) {
+  console.log(thing2);
+}
+
+console.log("Fourth thing done!");
